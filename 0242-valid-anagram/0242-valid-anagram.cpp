3940d43f5321321120f20fb6259c6ce401f33ab2
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2022-11-07 16:58:04 +0530

    Time: 17 ms (66.87%), Space: 7.2 MB (82.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -9,10 +9,9 @@ public:
         }
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i])
-                return false;
-        }
-        return true;
+        if(s==t)
+            return true;
+        else
+            return false;
     }
 };

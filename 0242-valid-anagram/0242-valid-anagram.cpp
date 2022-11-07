70964e80e2db260215b3a1d04b18bda020c2e4d4
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2022-11-07 17:01:01 +0530

    Time: 33 ms (16.83%), Space: 7.4 MB (56.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,8 +4,8 @@ public:
         if(s.size()!=t.size())
             return false;
         for(int i=0;i<s.size();i++){
-            s[i]=0+s[i];
-            t[i]=0+t[i];
+            s[i]=int(s[i]);
+            t[i]=int(t[i]);
         }
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());

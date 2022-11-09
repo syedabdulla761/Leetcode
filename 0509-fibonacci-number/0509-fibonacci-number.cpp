db106eeb2d14dc10cc1e0763b9e1448011923b71
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2022-11-09 12:42:18 +0530

    Time: 33 ms (20.61%), Space: 5.7 MB (95.97%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)
-        { 
-            return 0;
-        }
-        else if(n==1)
-        {    
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
+        return n==0?0:n==1?1:fib(n-1)+fib(n-2);
     }
 };

444effd86beac091dbe6fb6cfdfd8ef08589ec30
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2022-11-09 12:40:47 +0530

    Time: 27 ms (30.61%), Space: 6 MB (37.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -9,10 +9,6 @@ public:
         {    
             return 1;
         }
-        if(n>2)
-        {
-            return fib(n-1)+fib(n-2);
-        }
-        return 0;
+        return fib(n-1)+fib(n-2);
     }
 };

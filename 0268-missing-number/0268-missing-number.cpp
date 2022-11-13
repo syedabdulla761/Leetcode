b4a89a08a85ebd25691445cfb24beb93f83262bf
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2022-11-13 19:16:50 +0530

    Time: 77 ms (6.83%), Space: 18 MB (65.24%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int tot=nums.size()*(nums.size()+1)/2;
+        int su=0;
+        return tot-accumulate(nums.begin(),nums.end(),su);
+        
+    }
+};

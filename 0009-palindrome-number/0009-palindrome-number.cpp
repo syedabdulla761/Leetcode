27a4f097cd2e9f12ea8541182ba7058eb6fdb994
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2022-11-29 21:33:46 +0530

    Time: 47 ms (14.27%), Space: 5.9 MB (32.82%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string y=to_string(x);
-        if(x<0)return false;
         for(int i=0;i<y.size();i++){
             if(y[i]!=y[y.size()-1-i])
                 return false;

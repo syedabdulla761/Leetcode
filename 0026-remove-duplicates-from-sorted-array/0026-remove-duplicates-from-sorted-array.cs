687e36c89c0ca5a258feb118f8a70e215bb0265f
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2023-03-12 23:34:25 +0530

    Time: 148 ms (73.48%), Space: 46.4 MB (9.17%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -1,13 +1,12 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        Dictionary<int,int> map=new Dictionary<int,int>();
-        for(int i=0;i<nums.Length;i++){
-            map[nums[i]]=1;
+       var set=new HashSet<int>(nums);
+        int i=0;
+        foreach(var num in set){
+            nums[i]=num;
+            i++;
         }
-        for(int i=0;i<map.Count;i++){
-            nums[i]=map.Keys.ElementAt(i);
-        }
-        Array.Resize(ref nums,map.Count);
+        Array.Resize(ref nums,set.Count);
         return nums.Length;
     }
 }

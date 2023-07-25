61c2c48a76214946523c231eb5497e61f39ac552
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2023-07-25 20:56:11 +0530

    Time: 187 ms (21.28%), Space: 58.9 MB (67.10%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cs
+++ 0189-rotate-array/0189-rotate-array.cs
@@ -1,8 +1,18 @@
 public class Solution {
-    public void Rotate(int[] nums, int k) {
-    k %= nums.Length;
-    Array.Reverse(nums);
-    Array.Reverse(nums, 0, k);
-    Array.Reverse(nums, k, nums.Length - k);
-}
+    public void Rotate(int[] nums, int k) {        
+        // Calculate the effective rotation index
+        k = k % nums.Length;
+
+        // Create a temporary array to hold the rotated elements
+        int[] rotated = new int[nums.Length];
+
+        // Part 1: Take elements from index nums.Length - k to the last index
+        Array.Copy(nums, nums.Length - k, rotated, 0, k);
+
+        // Part 2: Take elements from the beginning up to index nums.Length - k and copy to rotated array
+        Array.Copy(nums, 0, rotated, k, nums.Length - k);
+
+        // Copy the rotated array back to the original 'nums' array
+        Array.Copy(rotated, nums, nums.Length);
+    }
 }

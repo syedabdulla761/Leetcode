ef6032f44a978f959db5c6bbb286ad64b95ed971
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2023-09-13 15:17:42 +0530

    Time: 2 ms (69.29%), Space: 40.6 MB (75.98%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,11 +1,9 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            result ^= s.charAt(i);
-            result ^= t.charAt(i);
-        }
-        result ^= t.charAt(t.length() - 1);
-        return result;
+       char result = 0;
+       for (int i = 0; i < s.length(); i++) {
+         result ^= s.charAt(i) ^ t.charAt(i);
+       }
+     return (char)(result ^ t.charAt(t.length() - 1));
     }
 }

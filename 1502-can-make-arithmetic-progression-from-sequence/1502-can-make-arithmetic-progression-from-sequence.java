949a8f7305040d918800669406eb36cedc9d1566
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2023-12-18 21:17:11 +0530

    Time: 2 ms (97.71%), Space: 41.3 MB (17.42%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -3,9 +3,7 @@ class Solution {
         Arrays.sort(arr);
         if(arr.length==1 || arr.length==2) return true;
         int r = arr[arr.length-1]-arr[arr.length-2];
-        System.out.println(r);
         for ( int i=arr.length-2 ; i>0;i--){
-            System.out.println(arr[i]-arr[i-1]);
             if((arr[i]-arr[i-1])!=r)return false;
         }
         return true;

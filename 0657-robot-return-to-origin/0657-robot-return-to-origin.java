11f1019df54a0da846d5c28cec2765a2df919fd7
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2024-01-04 23:18:02 +0530

    Time: 4 ms (98.54%), Space: 44.9 MB (5.09%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        Map<Character, Integer> frequencyMap = new HashMap<>();
-        
-    for( char c : moves.toCharArray()){
-        if ( Character.isLetter(c)){
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+       int countR = 0, countL = 0, countU = 0, countD = 0;
+
+        for (char c : moves.toCharArray()) {
+            if (c == 'R') {
+                countR++;
+            } else if (c == 'L') {
+                countL++;
+            } else if (c == 'U') {
+                countU++;
+            } else if (c == 'D') {
+                countD++;
+            }
         }
-    }
-    
-    int value = Math.abs(frequencyMap.getOrDefault('R', 0)- frequencyMap.getOrDefault('L', 0)) + Math.abs(frequencyMap.getOrDefault('D', 0)- frequencyMap.getOrDefault('U', 0));
-        
-        return value!=0? false : true;
+
+        return countR == countL && countU == countD;   
     }
 }

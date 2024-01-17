c25ff2cee73f4e83ce6ecaf30dc19b13f50653c4
Syed Abdulla <60843280+syedabdulla761@users.noreply.github.com>
2024-01-17 20:23:46 +0530

    Time: 419 ms (9.28%), Space: 44.3 MB (69.42%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        String repeat=s+s;
-        return repeat.substring(1,repeat.length()-1).indexOf(s)>=0;
-                              }
+         return s.matches("(.+)\\1+");
+    }
 }
